c2100bb04dc2cfb1b1bb42a8717eba8ed53230a5
nirm44n <nirmaan.bajpai@gmail.com>
2018-05-19 01:50:33 -0500

    implemented C++ Object
    *Completed chatserver.cpp with functions

--- chatserver.cpp
+++ chatserver.cpp
@@ -2,18 +2,28 @@
 #include <QDebug>
 
 ChatServer::ChatServer(QObject *parent) : QObject(parent) {
-
+	this -> msg = "";
 
 }
 
 QString ChatServer:: nirmaanUpdate(QString msg){
-  
+	this -> msg.append("Nirmaan: ");
+    this -> msg.append(msg);
+    this -> msg.append("\n");
+    qDebug() << "Tag1: " << this -> msg;
+    emit messageTextChanged();
+    return this -> msg;
 }
 
 QString ChatServer::codeChallengeUpdate(QString msg){
- 
+	this -> msg.append("CodeChallenge: ");
+    this -> msg.append(msg);
+    this -> msg.append("\n");
+    qDebug() << "Tag2: " << this -> msg;
+    emit messageTextChanged();
+    return this -> msg;
 }
 
 QString ChatServer::messageText(){
-    return ""
+    return this -> msg;
 }
